7ccdf5e42a3b0fc7b2ece46155f40ee71804e0da
planilhadedevolucao <planilhadedevolucao@hotmail.com>
2025-03-22 00:26:55 -0300

    PLANILHA ATUALIZADA
    planilha 22/03/2025

---
+++ script.js
@@ -0,0 +1,28 @@
+const form = document.getElementById('form-notas');
+const adicionarButton = document.getElementById('adicionar');
+const totalNotasEmitidasElement = document.getElementById('total-notas-emitidas');
+const totalNotasDevolvidasElement = document.getElementById('total-notas-devolvidas');
+const porcentagemTotalElement = document.getElementById('porcentagem-total');
+
+let totalNotasEmitidas = 0;
+let totalNotasDevolvidas = 0;
+
+adicionarButton.addEventListener('click', (e) => {
+  e.preventDefault();
+
+ 
+  const notasEmitidas = parseInt(document.getElementById('notas-emitidas').value);
+  const notasDevolvidas = parseInt(document.getElementById('notas-devolvidas').value);
+
+  if (!isNaN(notasDevolvidas) && !isNaN(notasEmitidas) && !isNaN(notasDevolvidas)) {
+    totalNotasEmitidas += notasEmitidas;
+    totalNotasDevolvidas += notasDevolvidas;
+
+    totalNotasEmitidasElement.innerText = totalNotasEmitidas;
+    totalNotasDevolvidasElement.innerText = totalNotasDevolvidas;
+    const porcentagemTotal = (totalNotasDevolvidas / totalNotasEmitidas) * 100;
+    porcentagemTotalElement.innerText = `${porcentagemTotal.toFixed(2)}%`;
+  } else {
+    alert('Por favor, insira valores numéricos válidos.');
+  }
+});
