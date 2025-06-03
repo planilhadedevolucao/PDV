cbe09859830297bb8cf06a3b0d46e111a988a9c3
planilhadedevolucao <planilhadedevolucao@hotmail.com>
2025-06-03 19:09:05 -0300

    atualizacao de linha
    linhas

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-const form = document.getElementById('form-notas');
-const adicionarButton = document.getElementById('adicionar');
-const totalNotasEmitidasElement = document.getElementById('total-notas-emitidas');
-const totalNotasDevolvidasElement = document.getElementById('total-notas-devolvidas');
-const porcentagemTotalElement = document.getElementById('porcentagem-total');
+const form = document.getElementById("form-notas");
+const adicionarButton = document.getElementById("adicionar");
+const totalNotasEmitidasElement = document.getElementById(
+  "total-notas-emitidas"
+);
+const totalNotasDevolvidasElement = document.getElementById(
+  "total-notas-devolvidas"
+);
+const porcentagemTotalElement = document.getElementById("porcentagem-total");
 
 let totalNotasEmitidas = 0;
 let totalNotasDevolvidas = 0;
 
-adicionarButton.addEventListener('click', (e) => {
+adicionarButton.addEventListener("click", (e) => {
   e.preventDefault();
 
- 
-  const notasEmitidas = parseInt(document.getElementById('notas-emitidas').value);
-  const notasDevolvidas = parseInt(document.getElementById('notas-devolvidas').value);
+  document.getElementById("atualizar").addEventListener("click", function () {
+    location.reload();
+  });
 
-  if (!isNaN(notasDevolvidas) && !isNaN(notasEmitidas) && !isNaN(notasDevolvidas)) {
+  const notasEmitidas = parseInt(
+    document.getElementById("notas-emitidas").value
+  );
+  const notasDevolvidas = parseInt(
+    document.getElementById("notas-devolvidas").value
+  );
+
+  if (
+    !isNaN(notasDevolvidas) &&
+    !isNaN(notasEmitidas) &&
+    !isNaN(notasDevolvidas)
+  ) {
     totalNotasEmitidas += notasEmitidas;
     totalNotasDevolvidas += notasDevolvidas;
 
@@ -23,6 +38,6 @@ adicionarButton.addEventListener('click', (e) => {
     const porcentagemTotal = (totalNotasDevolvidas / totalNotasEmitidas) * 100;
     porcentagemTotalElement.innerText = `${porcentagemTotal.toFixed(2)}%`;
   } else {
-    alert('Por favor, insira valores numéricos válidos.');
+    alert("Por favor, insira valores numéricos válidos.");
   }
 });
